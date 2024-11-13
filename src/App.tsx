cd5e570597148999b175cd851c1d03168ec26d4e
mochenya <2827075398@qq.com>
2024-11-13 18:33:13 +0800

    add vehicle models

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import {Canvas} from "@react-three/fiber";
-import {Box, OrbitControls} from "@react-three/drei";
+import {Box, MeshDiscardMaterial, OrbitControls} from "@react-three/drei";
 import {Suspense} from "react";
 import LoadingScene from "./scenes/base/LoadingScene.tsx";
+import {VehicleRacerLow} from "./components/vehicles/VehicleRacerLow.tsx";
+import {Physics, RigidBody} from "@react-three/rapier";
+import {Perf} from "r3f-perf";
 
 export default function App() {
 
@@ -9,12 +12,18 @@ export default function App() {
     <>
       <Canvas className="w-full h-full touch-none" shadows={true}>
         <Suspense fallback={<LoadingScene/>}>
+          <Perf position="top-left"/>
           <OrbitControls/>
           <ambientLight intensity={0.5}/>
-          <directionalLight intensity={0.5} position={[0, 5, 5]} castShadow/>
-          <Box args={[1, 1, 1]} position={[0, 0, 0]} castShadow receiveShadow>
-            <meshStandardMaterial color="hotpink"/>
-          </Box>
+          <directionalLight intensity={1} position={[0, 5, 5]} castShadow/>
+          <Physics debug={true}>
+            <VehicleRacerLow position={[0, 0.5, 0]}/>
+            <RigidBody type="fixed" restitution={0}>
+              <Box args={[10, 0.4, 10]} position={[0, -0.2, 0]} receiveShadow>
+                <MeshDiscardMaterial/>
+              </Box>
+            </RigidBody>
+          </Physics>
         </Suspense>
       </Canvas>
     </>
